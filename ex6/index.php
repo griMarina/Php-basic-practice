a84211267432afd80a627bc6615f785e63e7f259
Marina Grigoreva <mgrigoreva12@gmail.com>
2022-03-07 16:19:31 +0200

    ~

--- ex6/index.php
+++ ex6/index.php
@@ -9,21 +9,37 @@ $menu = [
     "href" => "#3"],
     ["name" => "Меню 4",
     "href" => "#4"],
-]
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<ul>
-    <?php foreach ($menu as $submenu): ?>
-	<li><a href='<?=$submenu["href"]?>'><?=$submenu["name"]?></a></li>
-    <?php endforeach; ?>
-</ul>
-</body>
-</html>
+];
+
+include "menu.php";
+
+/*  
+Пыталась сделать вложенное меню, но не осилила :(
+
+    $menu = [
+    ["name" => "Меню 1",
+    "href" => "#1"],
+    [
+        ["name" => "Меню 2.1",
+        "href" => "#2_1"],
+        ["name" => "Меню 2.2",
+        "href" => "#2_2"],
+        ["name" => "Меню 2.3",
+        "href" => "#2_3"]
+    ],
+    ["name" => "Меню 3",
+    "href" => "#3"],
+    ["name" => "Меню 4",
+    "href" => "#4"],
+];
+
+function renderMenu($menu) {
+    foreach ($menu as $submenu) {
+        if (is_array($submenu)) {
+            renderMenu($submenu);
+        } else {?>
+            <li><a href='<?=$menu["href"]?>'><?=$menu["name"]?></a></li>
+        <?php }
+    }
+}
+*/
